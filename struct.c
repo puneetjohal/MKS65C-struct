3e398396fbe07cfe286b33037943742510733be8
Puneet <pjohal@stuy.edu>
2018-10-03 21:20:24 -0400

    value modifier

--- struct.c
+++ struct.c
@@ -27,7 +27,7 @@ void printstr(struct foo s){
 //modifies value of struct foo.w randomly
 void switchWeapon(struct foo s){
   srand( time(NULL) );
-  s.w = '\0' //resets string
+  s.w = '\0'; //resets string
   strcpy(s.w, wpns[rand() % 6]);
 }
 
